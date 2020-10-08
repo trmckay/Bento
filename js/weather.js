dbdd8cfab5f53cd915104be8ca61581c26b120ec
Trevor McKay <trs.mckay@gmail.com>
2020-10-08 13:47:08 -0700

    fahrenheit

--- js/weather.js
+++ js/weather.js
@@ -5,11 +5,11 @@ const descElement = document.querySelector(".temperature-description p");
 // App data
 const weather = {};
 weather.temperature = {
-  unit: "celsius",
+  unit: "fahrenheit",
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = "C";
+var tempUnit = "F";
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
